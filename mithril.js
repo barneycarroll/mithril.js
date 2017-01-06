a89926731a333f59374c76f59b9e39e4d10fbae1
Barney Carroll <barney.carroll@gmail.com>
2017-01-06 17:16:43 +0000

    Compile Mithril from e1ea282

--- mithril.js
+++ mithril.js
@@ -430,8 +430,7 @@ var coreRenderer = function($window) {
 		return element
 	}
 	function createComponent(vnode, hooks, ns) {
-		// For object literals since `Vnode()` always sets the `state` field.
-		if (!vnode.state) vnode.state = Object.create(vnode.tag)
+		vnode.state = Object.create(vnode.tag)
 		var view = vnode.tag.view
 		if (view.reentrantLock != null) return $emptyFragment
 		view.reentrantLock = true
